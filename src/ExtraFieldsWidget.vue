1c8ee1763901914633940f9c11841407054671bf
Christian Klinger <ck@novareto.de>
2018-03-19 16:09:50 +0100

    more working

--- src/ExtraFieldsWidget.vue
+++ src/ExtraFieldsWidget.vue
@@ -1,41 +1,76 @@
 <template>
   <div>
-    <input type="text" name="form.field.extra_questions" v-bind:value="vv">
-    <h1>DAS IST EIN TEST</h1>
+    <input type="hidden" name="form.field.extra_questions" v-bind:value="vv">
+
+    <table class="table">
+      <tr>
+          <th>Frage</th>
+          <th>Typ</th>
+          <th>Antwort(en)</th>
+      </tr>
+
+      <tr v-for="q in questions" :key="q.question">
+          <td> {{q.question}} </td>
+          <td> {{q.type}}</td>
+          <td> <ul v-for="a in q.answers" :key="a.value"> <li>{{a.value}}</li> </ul> </td>
+      </tr>
+    </table>
     {{questions}}
-    <form @submit.prevent="save_question">
-      <div class="form-group">
-        <label for="exampleInputName2">Frage</label>
-        <input v-model="question.question"
+
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Zusatzfragen hinzufügen
+</button>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="myModal" :class="{ in: modalShown }">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Zusatzfrage erfassen</h4>
+      </div>
+      <div class="modal-body">
+       <form @submit.prevent="save_question">
+         <div class="form-group">
+          <label for="exampleInputName2">Frage</label>
+          <input v-model="question.question"
                type="text" class="form-control"
-               id="exampleInputName2" placeholder="Jane Doe" />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputEmail2">Type</label>
-        <select v-model="question.type" v-on:change="may_need_answers" class="form-control" id="exampleInputEmail2">
-    <option value="choice" selected="selected">Select in values</option>
-    <option value="multi">Select several in values</option>
-    <option value="bool">True or False</option>
-</select>
-    </div>
-      <div class="form-group" v-if="question.need_answers">
-        <label for="exampleInputEmail2">Answers</label>
-          <div v-for="answer in question.answers" v-bind:key="answer">
-        <input v-model="answer.value" />
-        <button class="btn btn-default"
-            v-on:click.prevent="remove_answer(answer)" >remove</button>
-      </div>
+               id="exampleInputName2" placeholder="Bitte geben Sie hier Ihre Frage ein" />
+         </div>
+         <div class="form-group">
+           <label for="exampleInputEmail2">Type</label>
+           <select v-model="question.type" v-on:change="may_need_answers" class="form-control" id="exampleInputEmail2">
+             <option value="choice" selected="selected">Einen Wert auswählen</option>
+             <option value="multi">Mehrere Werte auswählen</option>
+             <option value="bool">Wahr oder Falsch</option>
+           </select>
+         </div>
+         <div class="form-group" v-if="question.need_answers">
+           <label for="exampleInputEmail2">Antworten</label>
+            <div v-for="answer in question.answers" v-bind:key="answer">
+              <input v-model="answer.value" />
+              <button class="btn btn-default" v-on:click.prevent="remove_answer(answer)">
+                  <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+              </button>
+         </div>
         
-        <button type="submit" class="btn btn-default"
-        v-on:click.prevent="add_answer" >Add answer</button>
+        <button type="submit" class="btn btn-default" v-on:click.prevent="add_answer">Antwort hinzufügen</button>
       </div>
       <hr />
       <div>
-    <button type="submit" class="btn btn-default">Add Question</button>
-    <button type="submit" class="btn btn-default"
-        v-on:click.prevent="save_all">Save</button>
+         <button type="submit" class="btn btn-default">Frage Speichern</button>
       </div>
     </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Schließen</button>
+        <button type="submit" class="btn btn-default" v-on:click.prevent="save_all">Speichern</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
   </div>
 </template>
 
@@ -58,6 +93,9 @@ function check_answer(answer) {
 
 export default {
     methods: {
+        showModal() {
+            this.modalShown = !this.modalShown;
+        },
         save_all() {
             console.log('SAVE A-ll')
             var ss = ""
@@ -136,6 +174,7 @@ export default {
     data() {
     return {
             vv: '',
+            modalShown: false,
             questions: [],
             question: QUESTION
     }
